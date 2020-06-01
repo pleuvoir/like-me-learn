f833fee89e378dd4558b70308b004e890caba8fd
pleuvoir <pleuvior@foxmail.com>
2020-06-01 21:44:38 +0800

    continue

--- componment/cards_qthread.py
+++ componment/cards_qthread.py
@@ -4,6 +4,7 @@
 from PyQt5.QtCore import QThread, pyqtSignal
 
 from tools.config import Const
+import os
 
 
 class CardsThread(QThread):
@@ -13,7 +14,8 @@ class CardsThread(QThread):
         self.attach_signal = attach_signal
 
     def run(self):
-        with open(Const.knowledge_folder_path, 'r', encoding='utf-8') as md:
-            for line in md.readlines():
-                line_split = line.split('&&')
-                self.attach_signal.emit(line_split[0], line_split[1])
+        if os.path.exists(Const.knowledge_folder_path):
+            with open(Const.knowledge_folder_path, 'r', encoding='utf-8') as md:
+                for line in md.readlines():
+                    line_split = line.split('&&')
+                    self.attach_signal.emit(line_split[0], line_split[1])
